260293d556bbd4e5980b5caed1f06e4aa248c6d7
panth <stevenusmc161@gmail.com>
2020-09-24 16:25:16 -0400

    TAG error on page 470

---
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,53 @@
+package com.raywenderlich.podplay.ui
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.ItunesService
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+// 1
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+// 2
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+// 3
+        val searchManager = getSystemService(Context.SEARCH_SERVICE)
+                as SearchManager
+// 4
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(componentName))
+        return true
+    }
+
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+        itunesRepo.searchByTerm(term) {
+            Log.i(TAG, "Results = $it")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?:
+            return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+}
