5ea6a33f4c7a739380f9e9539aff1a3e81b02ac3
panth <stevenusmc161@gmail.com>
2020-09-24 17:31:08 -0400

    Please let me know if this runs on your device

---
+++ app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
@@ -0,0 +1,47 @@
+package com.raywenderlich.podplay.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.PodcastResponse
+
+class SearchViewModel(application: Application) :
+    AndroidViewModel(application) {
+
+    var iTunesRepo: ItunesRepo? = null
+
+    data class PodcastSummaryViewData(
+        var name: String? = "",
+        var lastUpdated: String? = "",
+        var imageUrl: String? = "",
+        var feedUrl: String? = "")
+
+    private fun itunesPodcastToPodcastSummaryView(
+        itunesPodcast: PodcastResponse.ItunesPodcast):
+            PodcastSummaryViewData {
+        return PodcastSummaryViewData(
+            itunesPodcast.collectionCensoredName,
+            itunesPodcast.releaseDate,
+            itunesPodcast.artworkUrl30,
+            itunesPodcast.feedUrl)
+    }
+
+    // 1
+    fun searchPodcasts(term: String,
+                       callback: (List<PodcastSummaryViewData>) -> Unit) {
+// 2
+        iTunesRepo?.searchByTerm(term) { results ->
+            if (results == null) {
+// 3
+                callback(emptyList())
+            } else {
+// 4
+                val searchViews = results.map { podcast ->
+                    itunesPodcastToPodcastSummaryView(podcast)
+                }
+// 5
+                callback(searchViews)
+            }
+        }
+    }
+}
